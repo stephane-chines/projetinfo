76d8c40f1739bc3b1422470799b35d89aa984530
stephane-chines <stephane.chines@student.junia.com>
2025-06-01 19:19:15 +0200

    Add files via upload

---
+++ public/Inscription/Inscription.js
@@ -0,0 +1,36 @@
+const Email = document.querySelector('#email');
+const Password = document.querySelector('#password');
+const ConfirmPassword = document.querySelector('#confirmpassword');
+const BoutonRetour= document.querySelector('#retour');
+const BoutonInscription= document.querySelector('#Inscription');
+
+
+BoutonInscription.addEventListener('click', () => { 
+    const mail = Email.value.trim();
+    const motdepasse = Password.value.trim();
+    const confirmMotdepasse = ConfirmPassword.value.trim();
+    const nom_complet = mail.split('@')[0];
+    const prenom = nom_complet.split('.')[0];
+    let nom = nom_complet.split('.')[1];
+    if (nom.includes('1') || nom.includes('2') ) {
+        nom= nom.replace(/[1-2]/g, '');
+    }
+    if (mail.includes('junia.com') && motdepasse === confirmMotdepasse) {
+        fetch('/api/inscription', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email : mail, motdepasse: motdepasse, prenom: prenom, nom: nom })
+    })
+    .then(res => res.json())
+    .then(data => {
+        window.location.href = 'http://localhost:3000/Connexion/Connexion.html';
+    });         
+}   
+        Email.value="";
+        Password.value="";
+    })
+
+
+BoutonRetour.addEventListener('click', () => {
+    window.location.href = 'http://localhost:3000/Connexion/Connexion.html';
+})
