6761ab425dc80bcc16192d315e0e8f49d2633af4
Stéphane CHINES <stephane.chines@student.junia.com>
2025-06-04 13:37:56 +0200

    console log pour api vote

--- server.js
+++ server.js
@@ -450,6 +450,7 @@ async function startServer() {
 
   app.post('/api/vote', async (req, res) => {
     const IDQuestion = parseInt(req.body.IDQuestion, 10);
+    console.log("Body reçu :", req.body);
     if (isNaN(IDQuestion)) return res.status(400).json({ error: "IDQuestion invalide" });
 
     try {
